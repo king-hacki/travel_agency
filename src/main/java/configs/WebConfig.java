035deb98177badd2e638e71ebbcc992b972d0af4
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-08-20 17:54:25 +0300

    create props file for WebConfig and relalize

--- src/main/java/configs/WebConfig.java
+++ src/main/java/configs/WebConfig.java
@@ -1,26 +1,41 @@
 package configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "controllers")
+@PropertySource("classpath:web.properties")
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${web.resource.path}")
+    String resourcePath;
+    @Value("${web.resource.location}")
+    String resourcesLocation;
+    @Value("${web.prefix}")
+    String prefix;
+    @Value("${web.suffix}")
+    String suffix;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
+        registry.addResourceHandler(resourcePath).
+                addResourceLocations("/pages/");
     }
 
     @Bean
     public InternalResourceViewResolver jspViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/pages/");
-        viewResolver.setSuffix(".jsp");
+        viewResolver.setPrefix(prefix);
+        viewResolver.setSuffix(suffix);
         return viewResolver;
     }
 }
