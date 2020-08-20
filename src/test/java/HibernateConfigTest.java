6dfa64a94be2a693f5b0212baf292f3eb9b06144
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-08-20 11:16:10 +0300

    test for transaction commit

--- src/test/java/HibernateConfigTest.java
+++ src/test/java/HibernateConfigTest.java
@@ -2,15 +2,18 @@ import configs.HibernateConfig;
 import models.Country;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TestTransaction;
 
 import javax.transaction.Transactional;
 
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -19,12 +22,8 @@ import static org.junit.Assert.assertNotNull;
 @Transactional
 public class HibernateConfigTest {
 
-    private SessionFactory sessionFactory;
-
     @Autowired
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    private SessionFactory sessionFactory;
 
     @Test
     public void testConnection() {
@@ -33,9 +32,54 @@ public class HibernateConfigTest {
         expected.setId(1L);
         expected.setName("Hotel");
         session.save(expected);
-        session.persist(expected);
         Country actual = session.find(Country.class, expected.getId());
         assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void transactionCommitTest() {
+        assertTrue(TestTransaction.isActive());
+
+        Session session = sessionFactory.getCurrentSession();
+        Country expected = new Country();
+        expected.setId(1L);
+        session.save(expected);
+        Country actual = session.find(Country.class, expected.getId());
+        assertNotNull(actual);
+        assertTrue(TestTransaction.isFlaggedForRollback());
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        assertFalse(TestTransaction.isFlaggedForRollback());
+        assertFalse(TestTransaction.isActive());
+
+        TestTransaction.start();
+        assertTrue(TestTransaction.isFlaggedForRollback());
+        assertTrue(TestTransaction.isActive());
+        session = sessionFactory.getCurrentSession();
+        actual = session.find(Country.class, expected.getId());
+        assertNotNull(actual);
+        session.delete(actual);
+        session.flush();
+        TestTransaction.end();
+        assertFalse(TestTransaction.isActive());
+
+        TestTransaction.start();
+        session = sessionFactory.getCurrentSession();
+        actual = session.find(Country.class, expected.getId());
+        assertNotNull(actual);
+        session.delete(actual);
+        session.flush();
+        assertTrue(TestTransaction.isActive());
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        assertFalse(TestTransaction.isActive());
+
+        TestTransaction.start();
+        assertTrue(TestTransaction.isActive());
+        session = sessionFactory.getCurrentSession();
+        actual = session.find(Country.class, expected.getId());
+        Assert.assertNull(actual);
     }
 
 }
