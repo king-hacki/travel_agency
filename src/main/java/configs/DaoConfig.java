c4449d668704721e9e424b73470c2506e7f4ab48
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-08-20 13:02:09 +0300

    config class for dao

---
+++ src/main/java/configs/DaoConfig.java
@@ -0,0 +1,26 @@
+package configs;
+
+
+import dao.CountryDao;
+import dao.HotelDao;
+import dao.impl.CountryDaoImpl;
+import dao.impl.HotelDaoImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "dao")
+public class DaoConfig {
+
+    @Bean
+    public HotelDao hotelDao() {
+        return new HotelDaoImpl(countryDao());
+    }
+
+    @Bean
+    public CountryDao countryDao() {
+        return new CountryDaoImpl();
+    }
+
+}
