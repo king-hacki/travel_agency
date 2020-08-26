bf581bcbb2323e2e0de47ffcc99aa07ee4feee28
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-08-26 21:25:54 +0300

    more sql script

--- src/main/resources/sql/fill_country_and_hotel_table.sql
+++ src/main/resources/sql/fill_country_and_hotel_table.sql
@@ -15,13 +15,61 @@ VALUES (3, 'admin@gmail.com', 'admin', 'admin', '$2a$10$zGeEfbhaBlvowTg.t9M3SOG8
 
 INSERT INTO Country(id, name) VALUES (1, 'Ukraine');
 INSERT INTO Country(id, name) VALUES (2, 'USA');
+INSERT INTO Country(id, name) VALUES (3, 'Poland');
 
-INSERT INTO Hotel(id, name, country_id) VALUES (1, 'Ukraine_Hotel_01', 1);
-INSERT INTO Hotel(id, name, country_id) VALUES (2, 'Ukraine_Hotel_02', 1);
+INSERT INTO Hotel(id, name, country_id) VALUES (1, 'Lviv Hotel', 1);
+INSERT INTO Hotel(id, name, country_id) VALUES (2, 'Ivano-Frankivs Hotel', 1);
+INSERT INTO Hotel(id, name, country_id) VALUES (3, 'Kiev Hotel', 1);
+INSERT INTO Hotel(id, name, country_id) VALUES (4, 'Odessa Hotel', 1);
+INSERT INTO Hotel(id, name, country_id) VALUES (5, 'Ukraine Hotel', 1);
+INSERT INTO Hotel(id, name, country_id) VALUES (6, 'Nadiya Hotel', 1);
 
-INSERT INTO Room(id, level, number, hotel_id) VALUES (1, 'STANDARD', 100, 1);
-INSERT INTO Room(id, level, number, hotel_id) VALUES (2, 'STANDARD', 200, 1);
-INSERT INTO Room(id, level, number, hotel_id) VALUES (3, 'STANDARD', 300, 1);
-INSERT INTO Room(id, level, number, hotel_id) VALUES (4, 'STANDARD', 200, 2);
+INSERT INTO Hotel(id, name, country_id) VALUES (7, 'Varshava Hotel', 2);
+INSERT INTO Hotel(id, name, country_id) VALUES (8, 'Hostel', 2);
+INSERT INTO Hotel(id, name, country_id) VALUES (9, 'Poland Hotel', 2);
+
+INSERT INTO Hotel(id, name, country_id) VALUES (9, 'USA 1 Hotel', 3);
+INSERT INTO Hotel(id, name, country_id) VALUES (10, 'USA 2 Hotel', 3);
+INSERT INTO Hotel(id, name, country_id) VALUES (11, 'USA 3 Hotel', 3);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (1, 'STANDARD', 101, 1);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (2, 'STANDARD', 201, 1);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (3, 'STANDARD', 302, 1);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (4, 'STANDARD', 223, 1);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (5, 'STANDARD', 104, 2);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (6, 'STANDARD', 205, 2);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (7, 'STANDARD', 306, 2);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (8, 'STANDARD', 227, 2);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (9, 'STANDARD', 108, 3);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (10, 'STANDARD', 209, 4);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (11, 'STANDARD', 310, 5);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (12, 'STANDARD', 220, 6);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (13, 'STANDARD', 131, 6);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (14, 'STANDARD', 240, 7);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (15, 'STANDARD', 350, 7);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (16, 'STANDARD', 260, 7);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (17, 'STANDARD', 11, 8);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (18, 'STANDARD', 270, 8);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (19, 'STANDARD', 380, 9);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (20, 'STANDARD', 20, 9);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (21, 'STANDARD', 1, 9);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (22, 'STANDARD', 800, 10);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (23, 'STANDARD', 500, 10);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (24, 'STANDARD', 620, 10);
+
+INSERT INTO Room(id, level, number, hotel_id) VALUES (25, 'STANDARD', 191, 11);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (26, 'STANDARD', 222, 11);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (27, 'STANDARD', 333, 11);
+INSERT INTO Room(id, level, number, hotel_id) VALUES (28, 'STANDARD', 444, 11);
 
 COMMIT ;
